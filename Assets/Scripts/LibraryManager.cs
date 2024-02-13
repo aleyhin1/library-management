d28ad81b9cdb053f509da5fa5facbc440b943eb0
Batu <aleyhin1@gmail.com>
2024-02-14 01:42:13 +0300

    RaycastBlocker added to popups

--- Assets/Scripts/LibraryManager.cs
+++ Assets/Scripts/LibraryManager.cs
@@ -17,12 +17,12 @@ public class LibraryManager : MonoBehaviour
 
     private void PopulateTheLibrary()
     {
-        AddBookToLibrary("Crime and Punishment", "Dostoyevski", "978-0-330-25864-7", 50);
-        AddBookToLibrary("The Three Musketeers", "Alexandre Dumas", "232-4-234-5942-1", 55);
-        AddBookToLibrary("Sonnets", "William Shakespeare", "523-5-634-1234-5", 24);
-        AddBookToLibrary("The Captain's Daughter", "Alexander Pushkin", "534-5-525-6712-3", 23);
-        AddBookToLibrary("White Fang", "Jack London", "978-0-06-112008-4", 2);
-        AddBookToLibrary("Faust", "Goethe", "978-0-7432-7356-5", 88);
+        AddBookToLibrary("Crime and Punishment", "Dostoyevski", "9780330258647", 50);
+        AddBookToLibrary("The Three Musketeers", "Alexandre Dumas", "232423459421", 55);
+        AddBookToLibrary("Sonnets", "William Shakespeare", "523563412345", 24);
+        AddBookToLibrary("The Captain's Daughter", "Alexander Pushkin", "534552567123", 23);
+        AddBookToLibrary("White Fang", "Jack London", "978061120084", 2);
+        AddBookToLibrary("Faust", "Goethe", "9780743273565", 88);
     }
 
     public void AddBookToLibrary(string title, string author, string isbn, int copyCount)
